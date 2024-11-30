8399d2612a8c82a148dcee785bda3428d549f9fd
Luiz Eduardo Gomes <160649342+LuizEduardo20@users.noreply.github.com>
2024-11-29 21:22:35 -0300

    Add files via upload

---
+++ Nivel-01/08-interactive-rating-component/src/App.jsx
@@ -0,0 +1,10 @@
+import InteractiveRating from "./assets/Components/InteractiveRating";
+import "./App.css";
+
+export default function App() {
+	return (
+		<div className="App">
+			<InteractiveRating />
+		</div>
+	);
+}
